7e75dbc1cd5b9c2a9ad0cc851a92338c489b2969
ptx <3351626369@qq.com>
2026-08-16 16:54:03 +0800

    网关

--- hm-service/src/main/java/com/hmall/service/impl/CartServiceImpl.java
+++ hm-service/src/main/java/com/hmall/service/impl/CartServiceImpl.java
@@ -114,17 +114,17 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     }
 
     private void checkCartsFull(Long userId) {
-        int count = lambdaQuery().eq(Cart::getUserId, userId).count();
+        int count = Math.toIntExact(lambdaQuery().eq(Cart::getUserId, userId).count());
         if (count >= 10) {
             throw new BizIllegalException(StrUtil.format("用户购物车课程不能超过{}", 10));
         }
     }
 
     private boolean checkItemExists(Long itemId, Long userId) {
-        int count = lambdaQuery()
+        int count = Math.toIntExact(lambdaQuery()
                 .eq(Cart::getUserId, userId)
                 .eq(Cart::getItemId, itemId)
-                .count();
+                .count());
         return count > 0;
     }
 }
